6c45f40ca8e89e878f9933102ab7d21d727625c6
lennu <juhamatti.laaksonen@gmail.com>
2021-04-20 20:03:23 +0300

    Added player random numbers and possibility for player complaints

--- app.py
+++ app.py
@@ -1,40 +1,68 @@
 import random
 from base64 import b64encode, b64decode
 from flask import Flask
-from blockchain import blockchain_send, blockchain_print, GAME_INITIALIZE, GAME_START, GAME_END
+from blockchain import blockchain_send, blockchain_print, GAME_INITIALIZE, GAME_START, GAME_END, GAME_COMPLAINT
 import hashlib
 
+
 app = Flask(__name__)
+
 
 PUBLIC_KEY = 12345
 SECRET_KEY = 12345
 
+
 def encrypt(number):
     return number * PUBLIC_KEY
 
+
 def decrypt(number):
     return int(number / SECRET_KEY)
+
 
 game_id = 1
 encrypted_service_random = encrypt(random.randrange(0, 100))
 
 initialization_block = blockchain_send(
-    dict(type=GAME_INITIALIZE, game_id=str(game_id), encrypted_service_random=encrypted_service_random)
+    dict(type=GAME_INITIALIZE, game_id=game_id, encrypted_service_random=encrypted_service_random)
 )
+
+player_random_numbers = [
+    dict(player_id=1, random_number=5),
+    dict(player_id=2, random_number=6),
+    dict(player_id=3, random_number=7),
+    dict(player_id=4, random_number=8),
+    dict(player_id=5, random_number=9)
+]
+
 start_block = blockchain_send(
-    dict(type=GAME_START, game_id=str(game_id), player_random_numbers=[])
+    dict(type=GAME_START, game_id=game_id, player_random_numbers=player_random_numbers)
 )
 
-start_block_hash = start_block['hash']
-start_block_tx_id = start_block['content']['tx_id']
-initialization_block_service_random = initialization_block['content']['encrypted_service_random']
-
-random_number = (
-    int.from_bytes(bytes(start_block_hash, 'utf-8'), 'big') +
-    int.from_bytes(bytes(start_block_tx_id, 'utf-8'), 'big') +
-    decrypt(initialization_block['content']['encrypted_service_random'])
-) % 6
+complaint_block = None #blockchain_send(
+    #dict(type=GAME_COMPLAINT, game_id=game_id, player_random_number=dict(player_id=3, random_number=7))
+#)
 
 
-blockchain_send(dict(type=GAME_END, game_id=str(game_id), random_number=random_number, secret_key=SECRET_KEY))
+if initialization_block and start_block and not complaint_block:
+    initialization_block_service_random = initialization_block['content']['encrypted_service_random']
+
+    start_block_hash = start_block['hash']
+    start_block_tx_id = start_block['content']['tx_id']
+    start_block_player_random_numbers = 0
+    for player_random_number in start_block['content']['player_random_numbers']:
+        start_block_player_random_numbers += player_random_number['random_number']
+
+    random_number = (
+        int.from_bytes(bytes(start_block_hash, 'utf-8'), 'big') +
+        int.from_bytes(bytes(start_block_tx_id, 'utf-8'), 'big') +
+        decrypt(initialization_block['content']['encrypted_service_random'] + 
+        start_block_player_random_numbers)
+    ) % 6
+
+    if random_number == 0:
+        random_number = 6
+
+
+    blockchain_send(dict(type=GAME_END, game_id=game_id, random_number=random_number, secret_key=SECRET_KEY))
 blockchain_print()
--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 
 GAME_INITIALIZE = 'game_initialize'
 GAME_START = 'game_start'
+GAME_COMPLAINT = 'game_complaint'
 GAME_END = 'game_end'
 
 
@@ -44,28 +45,67 @@ def _initialize():
             json.dump([], json_file)
 
 
-def  _new_transaction(content):
+def _is_type_not_allowed(game_id, content_type):
+    game_content_type = None
+    for block in _read():
+        if  block['content']['game_id'] == game_id:
+            game_content_type = block['content']['type']
+
+    if content_type == GAME_INITIALIZE and game_content_type != None:
+        return True
+    elif game_content_type == GAME_END or game_content_type == GAME_COMPLAINT:
+        return True
+    elif game_content_type == content_type:
+        return True
+    else:
+        return False
+    
+
+
+def  _game_transaction(content):
     game_id = content['game_id']
     content_type = content['type']
-    for block in _read():
-        if  block['content']['game_id'] == game_id and block['content']['type'] == content_type:
-            print(f'Game {game_id}: already {content_type} ({block["hash"]})')
-            return block
+    if _is_type_not_allowed(game_id=game_id, content_type=content_type):
+        print(f'Game {game_id}: already {content_type}')
+        return None
 
     block = _commit(content)
-    print(f'Game {game_id}: {content_type} ({block["hash"]})')
+    print(f'Game {game_id}: {content_type}')
     return block
 
+
+def  _game_complaint(content):
+    game_id = content['game_id']
+    content_type = content['type']
+    if _is_type_not_allowed(game_id=game_id, content_type=content_type):
+        print(f'Game {game_id}: already {content_type}')
+        return None
+    for block in _read():
+        if  block['content']['game_id'] == game_id and block['content']['type'] == GAME_START:
+            if content['player_random_number'] in block['content']['player_random_numbers']:
+                block = _commit(content)
+                print(f'Game {game_id}: {content_type}')
+                return block
+
+    print(f'Game {game_id}: bad complaint')
+    return None
+
+
 def _run_smart_contracts(content):
-    if content['game_id'] and content['type']:
-        return _new_transaction(content)
+    if content['game_id'] and content['type'] in [GAME_INITIALIZE, GAME_START, GAME_END]:
+        return _game_transaction(content)
+    if content['game_id'] and content['type'] == GAME_COMPLAINT:
+        return _game_complaint(content)
     else:
         return null
+
 
 def blockchain_send(content):
     return _run_smart_contracts(content)
 
+
 def blockchain_print():
     print(json.dumps(_read(), indent=2, sort_keys=True))
+
 
 _initialize()
